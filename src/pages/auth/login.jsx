d3344b67dfda416333270d8e71c7c8c850e59a53
ronywahyuu <ronywahyuu@upi.edu>
2023-05-21 20:26:44 +0700

    add contoh props

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,8 +1,12 @@
+import Input from "../../components/input";
 
 const LoginPage = () => {
   return (
     <section className="bg-white">
       <div>Login Page</div>
+
+      {/* ini input component */}
+      <Input placeholder="Placeholder login"/>
     </section>
   );
 };
