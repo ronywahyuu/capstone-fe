b4f834447ffef3d89334739cd47020789d87286c
chndrwali <chndrwali@gmail.com>
2023-05-22 22:05:19 +0700

    membuat login page dan menambahkan gamber lives-matter.png

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Outlet } from 'react-router-dom'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { Outlet } from 'react-router-dom';
 // import './App.css'
 
 function App() {
-
   return (
     <>
-      <Outlet/>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
