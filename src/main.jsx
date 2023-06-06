2dd8ec493198e400e922ab2bc803bd0a7562b79a
Sindi Rinaldi <122659534+SindiRinaldi@users.noreply.github.com>
2023-06-06 23:38:08 +0700

    membuat form donasi (#10)
    Co-authored-by: Rony Wahyu <87479126+ronywahyuu@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import {
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
-import ErrorPage from "./error-page.jsx";
-import LoginPage from "./pages/auth/login.jsx";
-import MainLayout from "./pages/main-layout.jsx";
-import LandingPage from "./pages/landing-page.jsx";
-import RegisterPage from "./pages/auth/register.jsx";
-import AboutPage from "./pages/about.jsx";
-import MainPage from "./pages/main-page.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './error-page.jsx'
+import LoginPage from './pages/auth/login.jsx'
+import MainLayout from './pages/main-layout.jsx'
+import LandingPage from './pages/landing-page.jsx'
+import RegisterPage from './pages/auth/register.jsx'
+import AboutPage from './pages/about.jsx'
+import BlogPage from './pages/blog-page.jsx'
+import MainPage from './pages/main-page.jsx'
+import FormDonasi from './components/home/write.jsx'
+
 import { QueryClient, QueryClientProvider } from "react-query";
-import WritePost from "./components/home/write.jsx";
 
 const router = createBrowserRouter([
   {
@@ -61,12 +61,16 @@ const router = createBrowserRouter([
         element: <MainPage />,
       },
       {
-        path: "saved",
-        element: <MainPage />,
+        path: 'saved',
+        element: <MainPage/>
       },
-    ],
-  },
-]);
+      {
+        path: 'donasi',
+        element: <FormDonasi/>
+      }
+    ]
+  }
+])
 
 const queryClient = new QueryClient();
 
